6278551c5d3a206231aae5ae810e3d7eef2d839e
Sujay Mansingh <sujay@editd.com>
2014-10-13 17:19:23 +0100

    Update README with new owner.

--- setup.py
+++ setup.py
@@ -13,13 +13,13 @@ if __name__ == "__main__":
     setuptools.setup(
         name="jsond",
         version="0.0.1",
-        author="Sujay Mansingh",
-        author_email="sujay.mansingh@gmail.com",
+        author="EDITD",
+        author_email="engineering@editd.com",
         packages=setuptools.find_packages(),
         scripts=[],
-        url="https://github.com/sujaymansingh/jsond",
+        url="https://github.com/EDITD/jsond",
         license="LICENSE.txt",
         description="JSON (with dates)",
-        long_description="View the github page (https://github.com/sujaymansingh/jsond) for more details.",
+        long_description="View the github page (https://github.com/EDITD/jsond) for more details.",
         install_requires=REQUIREMENTS
     )
