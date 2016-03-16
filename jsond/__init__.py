ffdb4af84d7a5a329f2f1c71582a6d64b23c1aa4
Bob Renwick <bob@editd.com>
2016-03-16 11:57:54 +0000

    use six so we can use this lib efficiently in py2 and py3

--- jsond/__init__.py
+++ jsond/__init__.py
@@ -5,6 +5,7 @@ import json
 import datetime
 
 import dateutil.parser
+import six
 
 
 class JSONDateEncoder(json.JSONEncoder):
@@ -39,7 +40,7 @@ class JSONDateDecoder(json.JSONDecoder):
             return s
 
     def _decode(self, data):
-        if isinstance(data, str) and data.startswith('datetime:'):
+        if isinstance(data, six.string_types) and data.startswith('datetime:'):
             return self.str_to_datetime(data)
 
         if isinstance(data, list):
@@ -47,7 +48,7 @@ class JSONDateDecoder(json.JSONDecoder):
 
         if isinstance(data, dict):
             d = {}
-            for key, value in data.items():
+            for key, value in six.iteritems(data):
                 d[key] = self._decode(value)
             return d
 
